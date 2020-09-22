0ea4deae80ae5eefae97085a5ab30c21cb4de97b
Miroslav Adamec <miroslav.adamec@invia.cz>
2020-09-22 19:34:46 +0200

    Improve JobItemCollectionConverter

--- src/Mirecad.Veeam.O365.Sharp/Objects/DTOs/JobItemCollectionDto.cs
+++ src/Mirecad.Veeam.O365.Sharp/Objects/DTOs/JobItemCollectionDto.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections.Generic;
-using Mirecad.Veeam.O365.Sharp.Infrastructure.Converters;
+using Mirecad.Veeam.O365.Sharp.Converters;
 using Newtonsoft.Json;
 
 namespace Mirecad.Veeam.O365.Sharp.Objects.DTOs
 {
     [JsonConverter(typeof(JobItemCollectionConverter))]
-    public class JobItemCollectionResultDto
+    public class JobItemCollectionDto
     {
         public List<SiteJobItemDto> Sites { get; set; } = new List<SiteJobItemDto>();
         public List<PartialOrganizationJobItemDto> PartialOrganizations { get; set; } = new List<PartialOrganizationJobItemDto>();
         public List<UserJobItemDto> Users { get; set; } = new List<UserJobItemDto>();
         public List<GroupJobItemDto> Groups { get; set; } = new List<GroupJobItemDto>();
+
+        internal JobItemCollectionDto()
+        {
+        }
     }
 }
--- src/Mirecad.Veeam.O365.Sharp/Objects/Domain/JobItemCollection.cs
+++ src/Mirecad.Veeam.O365.Sharp/Objects/Domain/JobItemCollection.cs
@@ -4,12 +4,16 @@ using Mirecad.Veeam.O365.Sharp.Objects.DTOs;
 
 namespace Mirecad.Veeam.O365.Sharp.Objects.Domain
 {
-    [DataTransferObject(typeof(JobItemCollectionResultDto))]
-    public class JobItemCollectionResult
+    [DataTransferObject(typeof(JobItemCollectionDto))]
+    public class JobItemCollection
     {
         public List<SiteJobItem> Sites { get; set; } = new List<SiteJobItem>();
         public List<PartialOrganizationJobItem> PartialOrganizations { get; set; } = new List<PartialOrganizationJobItem>();
         public List<UserJobItem> Users { get; set; } = new List<UserJobItem>();
         public List<GroupJobItem> Groups { get; set; } = new List<GroupJobItem>();
+
+        internal JobItemCollection()
+        {
+        }
     }
 }
