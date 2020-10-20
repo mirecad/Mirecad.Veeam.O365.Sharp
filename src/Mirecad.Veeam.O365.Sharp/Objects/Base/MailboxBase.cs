09a522661cc68c86ba3000e82c8551a14aa548aa
mirecad <miroslav.adamec@hotmail.com>
2020-10-20 14:43:10 +0200

    Add support for mailbox export (#16)

---
+++ src/Mirecad.Veeam.O365.Sharp/Objects/Base/MailboxBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Mirecad.Veeam.O365.Sharp.Objects.Base
+{
+    public class MailboxBase
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsArchive { get; set; }
+    }
+}
