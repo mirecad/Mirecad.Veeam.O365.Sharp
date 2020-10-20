9e98584737dbd9f89c63527fa0f0d1cd349edda7
Miroslav Adamec <miroslav.adamec@invia.cz>
2020-10-20 15:13:49 +0200

    Expose new clients to veeam client interface

--- src/Mirecad.Veeam.O365.Sharp/IVeeamO365Client.cs
+++ src/Mirecad.Veeam.O365.Sharp/IVeeamO365Client.cs
@@ -5,11 +5,13 @@ namespace Mirecad.Veeam.O365.Sharp
     public interface IVeeamO365Client
     {
         IJobClient Jobs { get; }
+        IMailboxClient Mailboxes { get; }
         IOneDriveClient OneDrives { get; }
         IOrganizationClient Organizations { get; }
         IOrganizationUserClient OrganizationUsers { get; }
         IOrganizationSiteClient OrganizationSites { get; }
         IOrganizationGroupClient OrganizationGroups { get; }
         IBackupRepositoryClient BackupRepositories { get; }
+        ISharePointClient SharePoints { get; }
     }
 }
