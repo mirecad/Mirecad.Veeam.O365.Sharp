04b2213a3e5da80a5ea1a9a49e843529780ba532
Miroslav Adamec <miroslav.adamec@invia.cz>
2020-12-15 16:24:43 +0100

    Add ability to stop restore session

---
+++ src/Mirecad.Veeam.O365.Sharp/Clients/RestoreSessionClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Mirecad.Veeam.O365.Sharp.Infrastructure.Http;
+
+namespace Mirecad.Veeam.O365.Sharp.Clients
+{
+    public class RestoreSessionClient : IRestoreSessionClient
+    {
+        private readonly VeeamO365Client _baseClient;
+
+        public RestoreSessionClient(VeeamO365Client baseClient)
+        {
+            _baseClient = baseClient;
+        }
+
+        public async Task StopRestoreSessionAsync(string restoreSessionId, CancellationToken ct = default)
+        {
+            ParameterValidator.ValidateNotNull(restoreSessionId, nameof(restoreSessionId));
+
+            var bodyParameters = new BodyParameters()
+                .AddNullParameter("stop");
+
+            var url = $"restoresessions/{restoreSessionId}/action";
+            await _baseClient.PostAsync(url, bodyParameters, ct);
+        }
+    }
+}
--- src/Mirecad.Veeam.O365.Sharp/Objects/Domain/RestoreSession.cs
+++ src/Mirecad.Veeam.O365.Sharp/Objects/Domain/RestoreSession.cs
@@ -9,13 +9,25 @@ namespace Mirecad.Veeam.O365.Sharp.Objects.Domain
     [DataTransferObject(typeof(RestoreSessionDto))]
     public class RestoreSession : RestoreSessionBase
     {
+        private readonly VeeamO365Client _client;
+
         private VeeamLink<Organization> _linksOrganization;
         private VeeamLink<VeeamPagedResult<RestoreSessionEvent>> _linksRestoreSessionEvents;
+
+        public RestoreSession(VeeamO365Client client)
+        {
+            _client = client;
+        }
 
         public async Task<Organization> GetOrganizationAsync(CancellationToken ct = default)
             => await _linksOrganization.InvokeAsync(ct);
 
         public async Task<VeeamPagedResult<RestoreSessionEvent>> GetRestoreSessionEventsAsync(CancellationToken ct = default)
             => await _linksRestoreSessionEvents.InvokeAsync(ct);
+
+        public async Task StopRestoreSessionAsync(CancellationToken ct = default)
+        {
+            await _client.RestoreSessions.StopRestoreSessionAsync(Id, ct);
+        }
     }
 }
